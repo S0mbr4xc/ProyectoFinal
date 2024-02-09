9a04d1e485e1411d32689ff36a8baf6b0fba00bd
EduardoArce03 <eduardomaldonado2003ortiz@gmail.com>
2024-02-09 12:54:47 -0500

    add diseños a todo

--- src/app/pages/mostrar-fac/mostrar-fac.component.ts
+++ src/app/pages/mostrar-fac/mostrar-fac.component.ts
@@ -27,28 +27,29 @@ export class MostrarFacComponent implements OnInit{
     // Extraemos el
     const DATA = document.getElementById('frm');
     if (!DATA) {
-      console.error('No se encontró el elemento con ID "frm"');
-      return;
+        console.error('No se encontró el elemento con ID "frm"');
+        return;
     }
-  
+
     const doc = new jsPDF('p', 'pt', 'a4');
     const options = {
-      background: 'white',
-      scale: 9
+        background: 'white',
+        scale: 9
     };
+
     html2canvas(DATA, options).then((canvas) => {
-      const img = canvas.toDataURL('image/PNG');
-      // Add image Canvas to PDF
-      const bufferX = 1;
-      const bufferY = 2;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
+        const img = canvas.toDataURL('image/PNG');
+        // Add image Canvas to PDF
+        const bufferX = 10;
+        const bufferY = 250;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
     }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
     });
-  }
-  
+}
+
 }
